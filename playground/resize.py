152505936b94dbe806f7c12da445eb2e86b1aeb0
sudoskys <coldlando@hotmail.com>
2024-08-18 20:12:09 +0800

    :art: refactor(playground): update input file and output filename in resize script

--- playground/resize.py
+++ playground/resize.py
@@ -2,11 +2,11 @@ from telegram_sticker_utils import ImageProcessor
 
 sticker = ImageProcessor.make_sticker(
     input_name='sad',
-    input_data=open("报警.gif", 'rb').read(),
+    input_data=open("test1.webm", 'rb').read(),
     scale=512,
     master_edge='width',
 )
 print(sticker.sticker_type)
 print(sticker.emojis)
-with open(f"output.{sticker.file_extension}", 'wb') as f:
+with open(f"output_webm.{sticker.file_extension}", 'wb') as f:
     f.write(sticker.data)
